4aebd7528677562fdcc068bd2a542eb88c6c5599
SaiAbhinav <23135662+SaiAbhinav@users.noreply.github.com>
2023-12-03 09:29:46 -0500

    Update roberta_seq.sh script file

--- scripts/roberta_seq.sh
+++ scripts/roberta_seq.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# /user/HS502/yl02706/.conda/envs/lyc/bin/python3 /user/HS502/yl02706/frame_finder/roberta_seq.py roberta-base /user/HS502/yl02706/frame_finder/data/vua
 
 python3 frame_finder/roberta_seq.py roberta-base data/VUA20
 
-
-nohup python3 frame_finder/roberta_seq.py roberta-base data/VUA20 &
+# nohup python3 frame_finder/roberta_seq.py roberta-base data/VUA20 &
