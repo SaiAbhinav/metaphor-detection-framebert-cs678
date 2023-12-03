cbc9f19624102f8323b67616a7fe8976e6543d1c
SaiAbhinav <23135662+SaiAbhinav@users.noreply.github.com>
2023-12-03 09:30:20 -0500

    Update run.sh script file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
-# python main.py --data_dir data/VUA20 --task_name vua --model_type FrameMelbert --class_weight 3 --bert_model roberta-base --num_train_epoch 3 --train_batch_size 32 --learning_rate 3e-5 --warmup_epoch 2
-
 python main.py --data_dir data/VUA18 --task_name vua --model_type MELBERT --class_weight 3 --bert_model roberta-base --num_train_epoch 3 --train_batch_size 32 --learning_rate 3e-5 --warmup_epoch 2
 
-nohup python main.py --data_dir data/VUA18 --task_name vua --model_type MELBERT --class_weight 3 --bert_model roberta-base --num_train_epoch 3 --train_batch_size 32 --learning_rate 3e-5 --warmup_epoch 2&
+# nohup python main.py --data_dir data/VUA18 --task_name vua --model_type MELBERT --class_weight 3 --bert_model roberta-base --num_train_epoch 3 --train_batch_size 32 --learning_rate 3e-5 --warmup_epoch 2&
